2394dc2084dafca5cf5ec81ef541b83c0d158d44
christian-nieves <thechristiannieves@gmail.com>
2026-04-10 10:34:47 -0400

    questions 1, 2, 3 are finished

--- src/main/java/com/pluralsight/Hello.java
+++ src/main/java/com/pluralsight/Hello.java
@@ -1,4 +1,28 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Hello {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        sayHello();
+        sayGoodbye();
+        sayGoodMorning();
+
+        myScanner.close();
+    }
+
+    public static void sayHello() {;
+        System.out.println("Hello!");
+    }
+
+    public static void sayGoodbye() {
+        System.out.println("Goodbye!");
+    }
+
+    public static void sayGoodMorning() {
+        System.out.println("Good morning!");
+
+    }
 }
---
+++ src/main/java/com/pluralsight/Menu.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        displayMenu();
+        displayMenu();
+
+
+
+    }
+
+    public static void displayMenu() {
+        System.out.println("=== MENU ===");
+        System.out.println("1. Coffee - $3.99");
+        System.out.println("2. Tea - $2.99");
+        System.out.println("3. Cookie - $1.99");
+    }
+}
+
